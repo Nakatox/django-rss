bec05efa6aaa4ab69bcdb0747007569502c9e5d0
Fabian <fabian.zuo@supinternet.fr>
2021-10-19 12:04:12 +0200

    views for feedparser exemple

--- rss/views.py
+++ rss/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import feedparser
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+def parser(request) :
+    # link rss : view-source:https://daily.dev/blog
+    d = feedparser.parse(requests.get("https://www.clubic.com/feed/news.rss").text)
+    return HttpResponse(str(d['entries']))
