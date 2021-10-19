9a21f6b2e4b7e3c306f6f5028e1c3377b17c8faf
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2021-10-19 12:45:29 +0200

    Added link clickable admin

--- rss/admin.py
+++ rss/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-
 from rss.models import Article, Vote
 from django.db.models import Count
+from django.utils.html import format_html
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ["title", "link", "user", "created_at", "vote_count"]
+    list_display = ["title", "custom_rss_link", "user", "created_at", "vote_count"]
 
+    def custom_rss_link(self, obj):
+        return format_html(
+ '<a  href="{0}" >{0}</a>&nbsp;',
+            obj.link
+        )
+    
     def vote_count(self, obj):
         return obj._vote_count
 
