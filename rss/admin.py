bc5bde1ed741ee57b2a4b8318444e37f4af26c55
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2021-10-19 12:00:49 +0200

    Fix Article Model link length and Vote Model self definition

--- rss/admin.py
+++ rss/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from rss.models import Article
+from rss.models import Article, Vote
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ["title", "link", "user", "created_at"]
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ["user", "article"]
     
--- rss/models.py
+++ rss/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Article(models.Model):
-    title = models.CharField(max_length=50)
-    link = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    link = models.CharField(max_length=512)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
     def __str__(self):
@@ -13,3 +13,5 @@ class Article(models.Model):
 class Vote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Vote '{self.article}'"
